99a7f76f7ab90e9cf797a04bdf98dd93a20c7d86
hengwu <hackersk@qq.com>
2017-02-27 02:32:59 +0800

    init

---
+++ output/await2.js
@@ -0,0 +1,41 @@
+"use strict";
+
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
+var f = function f() {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            resolve(123);
+        }, 2000);
+    });
+};
+
+var testAsync = function () {
+    var _ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee() {
+        var t;
+        return regeneratorRuntime.wrap(function _callee$(_context) {
+            while (1) {
+                switch (_context.prev = _context.next) {
+                    case 0:
+                        _context.next = 2;
+                        return f();
+
+                    case 2:
+                        t = _context.sent;
+
+                        console.log(t);
+
+                    case 4:
+                    case "end":
+                        return _context.stop();
+                }
+            }
+        }, _callee, undefined);
+    }));
+
+    return function testAsync() {
+        return _ref.apply(this, arguments);
+    };
+}();
+
+testAsync();
